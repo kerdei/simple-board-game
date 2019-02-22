97a754bc7bacc92c821766f8e57c3cd17a2f4315
Krisztián Erdei <kerdei@gmail.com>
2019-02-22 12:54:12 +0100

    PrintClass,Gameplay refactor
    Adding PrintClass as a View Class
    
    Refactor Gameplay, making it to a Singleton

---
+++ src/main/java/hu/kerdei/uni/view/PrintClass.java
@@ -0,0 +1,66 @@
+package hu.kerdei.uni.view;
+
+import hu.kerdei.uni.gameobject.Player;
+import hu.kerdei.uni.gameobject.Pos;
+
+public class PrintClass {
+
+    public static void endMessage(String winnerPlayerName) {
+        System.out.println("==========================");
+        System.out.println("=========GAME OVER========");
+        System.out.println("==========================");
+        System.out.println();
+        System.out.println("WINNER PLAYER:" + winnerPlayerName);
+    }
+
+    public static void startMessage() {
+        System.out.println("Welcome to this simple board game"); //TODO
+    }
+
+    public static void askPlayer1Name() {
+        System.out.println();
+        System.out.println("First player, your color will be blue");
+        System.out.println("Please tell me your name");
+        System.out.println("Player1:");
+        System.out.println();
+    }
+
+    public static void askPlayer2Name() {
+        System.out.println();
+        System.out.println("Second player, your color will be red");
+        System.out.println("Please tell me your name");
+        System.out.println("Player2:");
+        System.out.println();
+    }
+
+    public static void infoAboutStart() {
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("The game about to start, here is some info:");
+        System.out.println("You can move your fields if you give the ''from'' positions and the ''to'' positions");
+        System.out.println("For example input : 0 0 1 0 , will result the (0,0) BLUE field will move to the (0,1) empty field");
+        System.out.println();
+    }
+
+    public static void nextPlayerShouldMakeMove(Player player) {
+        System.out.println("Please give your next move, " + player.getName() + " (" + player.getColor().name() + ")");
+    }
+
+    public static void madeMove(Pos from, Pos to) {
+        System.out.println("You made a move from (" + from.row + "," + from.column + ") to (" + to.row + "," + to.column + ")");
+    }
+
+    public static void pressAnyKey() {
+        System.out.println("Press Enter to continue...");
+    }
+
+    public static void printBoard(String boardInReadableFormat) {
+        System.out.println(boardInReadableFormat);
+    }
+
+    public static void wrongMove() {
+        System.out.println("This move is not allowed");
+    }
+}
--- src/test/java/hu/kerdei/uni/test/GameObjectsTest.java
+++ src/test/java/hu/kerdei/uni/test/GameObjectsTest.java
@@ -14,8 +14,11 @@ import static org.junit.Assert.*;
 
 public class GameObjectsTest {
 
-    static final Logger LOG = LoggerFactory.getLogger(GameObjectsTest.class);
+    private static final Logger LOG = LoggerFactory.getLogger(GameObjectsTest.class);
 
+    public GameObjectsTest(){
+        LOG.debug("GameObjectsTest start");
+    }
 
     @Test
     public void boardConstructorShouldGiveBackTheStartState() {
@@ -251,12 +254,14 @@ public class GameObjectsTest {
 
     @Test
     public void startStateIsNotAFinalStateTest() {
+        LOG.debug("startStateIsNotAFinalStateTest");
         Board board = new Board();
         assertFalse(board.isFinalState());
     }
 
     @Test
     public void notFinalState1Test1() {
+        LOG.debug("notFinalState1Test1");
         Board board = new Board();
         board.switchFields(new Pos(4, 0), new Pos(3, 0));
         board.switchFields(new Pos(4, 2), new Pos(2, 1));
@@ -266,6 +271,7 @@ public class GameObjectsTest {
 
     @Test
     public void notFinalState1Test2() {
+        LOG.debug("notFinalState1Test2");
         Board board = new Board();
         board.switchFields(new Pos(0, 0), new Pos(2, 1));
         board.switchFields(new Pos(0, 2), new Pos(2, 2));
@@ -275,6 +281,7 @@ public class GameObjectsTest {
 
     @Test
     public void notFinalState1Test3() {
+        LOG.debug("notFinalState1Test3");
         Board board = new Board();
         board.switchFields(new Pos(0, 1), new Pos(1, 1));
         board.switchFields(new Pos(1, 2), new Pos(2, 2));
